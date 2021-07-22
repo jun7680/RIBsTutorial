4e6c523be00acc8b7727c5085614962c93f35260
옥인준 <injunock@mailplug.co.kr>
2021-07-22 15:27:39 +0900

    [+] Project create

---
+++ RibsTutorial/RibsTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RibsTutorial
+//
+//  Created by injun on 2021/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
