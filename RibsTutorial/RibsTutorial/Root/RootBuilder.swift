702f8a627e23b31a70d77a43da39c81b50c55244
옥인준 <injunock@mailplug.co.kr>
2021-07-23 10:04:09 +0900

    [+] LoggedOut Rib

--- RibsTutorial/RibsTutorial/Root/RootBuilder.swift
+++ RibsTutorial/RibsTutorial/Root/RootBuilder.swift
@@ -32,7 +32,7 @@ final class RootBuilder: Builder<RootDependency>, RootBuildable {
     func build() -> LaunchRouting  {
         let component = RootComponent(dependency: dependency)
         let viewController = RootViewController()
-        let interactor = RootInteractor(presenter: viewController)        
+        let interactor = RootInteractor(presenter: viewController)
         return RootRouter(interactor: interactor, viewController: viewController)
     }
 }
